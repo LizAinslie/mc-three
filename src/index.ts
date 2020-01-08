0371639feba97cbb0011775c7f8c7d9f5674cc61
Landon Gravat <railinator4903@gmail.com>
2020-01-08 21:06:41 +0000

    forgot about that xD

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import MinecraftModelLoader from "./model/MinecraftModelLoader";
-import MinecraftTextureLoader from "./texture/MinecraftTextureLoader";
-import MinecraftTexture from "./texture/MinecraftTexture";
+import MinecraftModelLoader from './model/MinecraftModelLoader';
+import MinecraftTextureLoader from './texture/MinecraftTextureLoader';
+import MinecraftTexture from './texture/MinecraftTexture';
 
+(window as any).MC = {};
 (window as any).MC.MinecraftModelLoader = MinecraftModelLoader;
 (window as any).MC.MinecraftTextureLoader = MinecraftTextureLoader;
 (window as any).MC.MinecraftTexture = MinecraftTexture;
